f3d3b22f8dc5c65b33ed8c7d37bd54cf7dfde7b7
José María Mateos <jmmateos@mce.hggm.es>
2013-10-16 16:04:33 +0200

    Added 'Results to Static' plugin

---
+++ src/main/java/limtools/Results_to_Static.java
@@ -0,0 +1,110 @@
+package limtools;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.gui.GenericDialog;
+import ij.measure.ResultsTable;
+import ij.plugin.PlugIn;
+import ij.plugin.filter.Analyzer;
+
+/**
+ * <p>
+ * This plugin creates a new 3D image with the values extracted from a 
+ * {@link ResultsTable} in a format very similar to that employed by the
+ * {@link Dynamic_to_Results} plugin.
+ * </p>
+ * 
+ * @author José María Mateos - jmmateos@hggm.es
+ * 
+ */
+public class Results_to_Static implements PlugIn {
+
+    @Override
+    public void run(String args) {
+
+        ResultsTable rt = Analyzer.getResultsTable();
+        
+        // Is the number of columns right?
+        int nCols = rt.getLastColumn();
+        if (nCols == -1) { 
+            IJ.error("The Results table is empty.");
+            return;
+        } else if (nCols < 3) {
+            IJ.error("The Results table does not contain enough columns.");
+            return;
+        }
+        
+        // Are the headings the right ones?
+        String [] headings = rt.getHeadings();
+        if (!headings[0].equals("x") || !headings[1].equals("y") 
+                || !headings[2].equals("slice")) {
+            IJ.error("The headings for the first three columns are wrong.");
+            return;
+        }            
+        
+        int [] x = _float2int(rt.getColumn(0));
+        int [] y = _float2int(rt.getColumn(1));
+        int [] slices = _float2int(rt.getColumn(2));
+        float [] param = rt.getColumn(3);
+        
+        int max_x = _getMax(x);
+        int max_y = _getMax(y);
+        int max_slice = _getMax(slices);
+        
+        // We need to ask the user for the image dimensions, as there is no
+        // way of guessing them from the coordinate values (the voxels have
+        // probably been masked beforehand)
+        GenericDialog gd = new GenericDialog("Please set the image dimensions");
+        gd.addNumericField("X", 0, 4);
+        gd.addNumericField("Y", 0, 4);
+        gd.addNumericField("Slices", 0, 4);
+        gd.showDialog();
+        
+        if (gd.wasCanceled())
+            return;
+        
+        int dim_x = (int)Math.round(gd.getNextNumber());
+        int dim_y = (int)Math.round(gd.getNextNumber());
+        int dim_slice = (int)Math.round(gd.getNextNumber());
+        
+        // Check that the user provided values are correct
+        if (dim_x < max_x || dim_y < max_y || dim_slice < max_slice) {
+            IJ.error("The provided values are lower than some of the " +
+                     "coordinates read from the Results table.");
+            return;
+        }
+        
+        // Build the image
+        ImagePlus imp = IJ.createImage("Results to Static image", dim_x, dim_y, 
+                                       dim_slice, 32);
+        ImageStack is = imp.getStack();
+        // Populate the stack
+        for (int i = 0; i < x.length; i++) {
+            is.setVoxel(x[i], y[i], slices[i] - 1, param[i]);
+        }
+        
+        // Display the image
+        imp.show(); 
+    }
+    
+    // Converts a float array into an integer array. This is used to cast the
+    // coordinate values.
+    private int [] _float2int(float [] data) {
+        int [] res = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            res[i] = Math.round(data[i]);
+        }
+        return res;
+    }
+    
+    // Gets the maximum integer value from an array.
+    private int _getMax(int [] data) {
+        int res = -Integer.MAX_VALUE;
+        for (int i : data) {
+            if (i > res)
+                res = i;
+        }
+        return res;
+    }
+}
